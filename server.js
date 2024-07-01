80bccb1d14b4397cd3984aeca20708d683d14b2b
DevonDD4S <devondevosd4s@gmail.com>
2024-07-01 16:11:17 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -93,14 +93,12 @@ app.use(async (req, res, next) => {
 });
 
 app.post('/send-email', (req,res) => {
+  let userEmail = decryptEmail()
   const {userName,userNumber,userSelect,userEmailText} = req.body
-  const userEmail = decryptEmail()
-  console.log('userEmail: ',userEmail)
-  console.log('req.session.passport.user: ',req.session.passport.user)
-  
   if (!userEmail) {
-    return res.status(401).send('User email not found or unauthorized.')
+    userEmail = req.body.userEmail
   }
+  console.log('userEmail: ',userEmail)
   sendEmail(userEmail,userName,userNumber,userSelect,userEmailText)
     .then(() => res.redirect('/getStarted'))
     .catch((error) => res.status(500).send(`Error: ${error.message}`))
