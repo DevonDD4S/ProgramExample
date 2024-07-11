31db965e5e461a957df8d76c8c399b57021d813b
DevonDD4S <devondevosd4s@gmail.com>
2024-07-11 18:56:08 +0200

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -3,18 +3,8 @@ $(document).ready(function(){let yearNow=new Date().getFullYear()
   const $items=$('.page');const $itUnderline=$('.itUnderline')
   function checkVisibility(){$items.each(function(){const $this=$(this);const offsetTop=$this.offset().top;const scrollTop=$(window).scrollTop();const windowHeight=$(window).height();if(offsetTop<scrollTop+windowHeight-50){$this.addClass('show');$itUnderline.addClass('underlinedIT')}})}
   $(window).on('scroll',checkVisibility);checkVisibility()
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#scrollToTopBtn').fadeIn()
-    } else {
-      $('#scrollToTopBtn').fadeOut()
-    }
-  })
-  $('#scrollToTopBtn').click(function() {
-    $('html, body').animate({scrollTop:0}, 10)
-    return false
-  })
-})
-var message=$('#message').text();var numberMessage=$('#numberMessage').text();if(message.length > 3){alert(message)}
-if(numberMessage.length > 3){alert(numberMessage)}
-
+  $(window).scroll(function(){if($(this).scrollTop()>100){$('#scrollToTopBtn').fadeIn()}else{$('#scrollToTopBtn').fadeOut()}})
+  $('#scrollToTopBtn').click(function(){$('html, body').animate({scrollTop:0},10)
+  return!1})})
+  var message=$('#message').text();var numberMessage=$('#numberMessage').text();if(message.length>3){alert(message)}
+  if(numberMessage.length>3){alert(numberMessage)}
